bf58335714692c84db5a1ec4a4a5cf69ed22b39c
Shuntaro Shimizu <ut.s.shimizu@gmail.com>
2022-02-08 15:31:24 +0900

    implement PF_PACKET device

---
+++ pkg/raw/pfpacket/pfpacket.go
@@ -0,0 +1,40 @@
+package pfpacket
+
+import "syscall"
+
+type PFPacket struct {
+	fd   int
+	name string
+}
+
+func NewPFPacket(name string) (*PFPacket, error) {
+	fd, err := openPFPacket(name)
+	if err != nil {
+		return nil, err
+	}
+	return &PFPacket{
+		fd:   fd,
+		name: name,
+	}, nil
+}
+
+func (p *PFPacket) Name() string {
+	return p.name
+}
+
+func (p *PFPacket) Address() []byte {
+	addr, _ := getAddress(p.name)
+	return addr[:6]
+}
+
+func (p *PFPacket) Read(b []byte) (int, error) {
+	return syscall.Read(p.fd, b)
+}
+
+func (p *PFPacket) Write(b []byte) (int, error) {
+	return syscall.Write(p.fd, b)
+}
+
+func (p *PFPacket) Close() error {
+	return syscall.Close(p.fd)
+}
